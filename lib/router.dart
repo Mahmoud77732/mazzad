4d8bbaee6dd79984e48405fd75bdbb1d22cbe4ff
HhmedHesham <heshama7793@gmail.com>
2022-03-14 16:30:24 +0200

    folders convention and router animation

---
+++ lib/router.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+// take the settings from onGenerateRoute in GetMaterial
+class Router {
+  static Route<dynamic> onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case "":
+        return SlideRoute(page: Container());
+
+      default:
+        return SlideRoute(page: Container());
+    }
+  }
+}
+
+enum SlideDirection { left, up, right, down }
+
+class SlideRoute extends PageRouteBuilder {
+  final SlideDirection direction;
+  final Widget page;
+  SlideRoute({
+    this.direction = SlideDirection.left,
+    required this.page,
+  }) : super(
+          pageBuilder: (
+            context,
+            animation,
+            secAnimation,
+          ) =>
+              page,
+          transitionsBuilder: (context, animation, secAnimation, child) =>
+              SlideTransition(
+            position: Tween<Offset>(
+              begin: direction == SlideDirection.left
+                  ? const Offset(1, 0)
+                  : direction == SlideDirection.right
+                      ? const Offset(-1, 0)
+                      : direction == SlideDirection.up
+                          ? const Offset(0, 1)
+                          : const Offset(0, -1),
+              end: Offset.zero,
+            ).animate(
+              CurvedAnimation(
+                parent: animation,
+                curve: Curves.easeInOut,
+              ),
+            ),
+            child: child,
+          ),
+        );
+}
